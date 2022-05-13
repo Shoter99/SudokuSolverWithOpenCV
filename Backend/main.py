1ed65833067843e52f5bb63dac68d644ec017185
Vivi <shoter998@gmail.com>
2022-05-13 17:07:36 +0200

    image added

--- Backend/main.py
+++ Backend/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
 from pydantic import BaseModel
 from PythonScripts.Sudoku import SolveSudoku
 import json
@@ -15,5 +15,12 @@ def home(board: Board):
 @app.get('/api/test')
 def test():
     return 'test'
+
+@app.post('/api/read_numbers')
+def read_numbers(file: UploadFile):
+    return {
+        'filename': file.filename,
+        'type': file.content_type,
+        }
 
 # [[" ", "2", " ", "4", " ", "6", " ", " ", " "], [" ", " ", " ", " ", " ", "5", " ", "7", " "], [" ", " ", " ", "1", " ", " ", " ", "3", " "], [" ", "1", " ", "8", " ", "2", " ", "9", " "], [" ", " ", "2", " ", "5", " ", " ", " ", " "], [" ", " ", " ", " ", " ", "7", "8", " ", " "], [" ", " ", " ", "5", " ", "8", "9", " ", " "], ["4", " ", " ", " ", " ", "6", " ", " ", " "], [" ", " ", "1", "4", " ", " ", " ", " ", " "]]

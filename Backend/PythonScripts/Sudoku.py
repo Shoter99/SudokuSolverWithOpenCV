4f641a9c30ae0f3ed40531542a31296e6259e896
Wikator <102376424+Wikator@users.noreply.github.com>
2022-05-14 13:38:39 +0200

    Update Sudoku.py

--- Backend/PythonScripts/Sudoku.py
+++ Backend/PythonScripts/Sudoku.py
@@ -7,147 +7,60 @@ def SolveSudoku(sudoku):
 
     groups = [[0, 1, 2], [3, 4, 5], [6, 7, 8]]
 
-    repeat = True
     numbersLeft = 81
-    pierwszyRaz = True
+    firstTime = True
             
     for i in range (9):
         for j in range (9):
             if sudoku[i][j] != ' ':
                 numbersLeft -= 1
+     
+    for attempt in range(5000):
+        
+        numberWasAdded = False
+        avaiblePlaces = {}
+        avaibleNumbers = {}
+        
+        for i in range (9):
+            for j in range(9):
+                avaiblePlaces[str(i)+str(j)] = 0
 
-           
-    while numbersLeft!=0:
-        while repeat:       
-            repeat = False
-            avaiblePlaces = {}
-            avaibleNumbers = {}
-            for i in range (9):
-                for j in range(9):
-                    avaiblePlaces[str(i)+str(j)] = 0
-                    avaibleNumbers[str(i)+str(j)] = 0
-                            
-            for number in numbers:
-                for i in range (9):
-                    if sudoku[i].count(number) == 0:
-                        places = 0
-                        placei = 0
-                        placej = 0
-                        for j in range (9):
-                            if sudoku[i][j] == ' ':
-                                works = True
-                                
-                                for i1 in range (groups[i//3][0], groups[i//3][2]+1):
-                                    for j1 in range(groups[j//3][0], groups[j//3][2]+1):
-                                        if sudoku[i1][j1] == number:
-                                            works = False
-
-                                for i2 in range (i%3, 9, 3):
-                                    for j2 in range(j%3, 9, 3):
-                                        if sudoku[i2][j2] == number:
-                                            works = False
-                                if works:
-                                    avaiblePlaces[str(i)+str(j)] += 1
-                                    avaibleNumbers[str(i)+str(j)] = number
-                                    places += 1
-                                    placei = i
-                                    placej = j
-
-                        if places == 1:
-                            repeat = True
-                            sudoku[placei][placej] = number
-                            numbersLeft -= 1
+        try:
             
+            for z in range(0, 9, 3):
+                for k in range (0, 9, 3):
+                    numberToAdd = list(numbers)
+                    for i in range (0+z, 3+z):
+                        for j in range (0+k, 3+k):
+                            if sudoku[i][j] != ' ':
+                                numberToAdd.remove(sudoku[i][j])
+                    if len(numberToAdd) == 1:
+                        for i in range (0+z, 3+z):
+                            for j in range (0+k, 3+k):
+                                if sudoku[i][j] == ' ':
+                                    numbersLeft -= 1
+                                    sudoku[i][j] = numberToAdd[0]
+                                    numberWasAdded = True
+                                
+            for z in range(3):
+                for k in range (3):
+                    numberToAdd = list(numbers)
+                    for i in range (0+z, 9, 3):
+                        for j in range (0+k, 9, 3):
+                            if sudoku[i][j] != ' ':
+                                numberToAdd.remove(sudoku[i][j])
+                    if len(numberToAdd) == 1:
+                        for i in range (0+z, 9, 3):
+                            for j in range (0+k, 9, 3):
+                                if sudoku[i][j] == ' ':
+                                    numbersLeft -= 1
+                                    sudoku[i][j] = numberToAdd[0]
+                                    numberWasAdded = True
+
+        except ValueError:
+
             try:
-                
-                for z in range(0, 9, 3):
-                    numberToAdd = list(numbers)
-                    for i in range (0+z, 3+z):
-                        for j in range (0, 3):
-                            if sudoku[i][j] != ' ':
-                                numberToAdd.remove(sudoku[i][j])
-                    if len(numberToAdd) == 1:
-                        for i in range (0+z, 3+z):
-                            for j in range (0, 3):
-                                if sudoku[i][j] == ' ':
-                                    numbersLeft -= 1
-                                    sudoku[i][j] = numberToAdd[0]
-                                    repeat = True
-
-                    numberToAdd = list(numbers)        
-                    for i in range (0+z, 3+z):
-                        for j in range (3, 6):
-                            if sudoku[i][j] != ' ':
-                                numberToAdd.remove(sudoku[i][j])
-                    if len(numberToAdd) == 1:
-                        for i in range (0+z, 3+z):
-                            for j in range (3, 6):
-                                if sudoku[i][j] == ' ':
-                                    numbersLeft -= 1
-                                    sudoku[i][j] = numberToAdd[0]
-                                    repeat = True
-
-
-                    numberToAdd = list(numbers)
-                    for i in range (0+z, 3+z):
-                        for j in range (6, 9):
-                            if sudoku[i][j] != ' ':
-                                numberToAdd.remove(sudoku[i][j])
-                    if len(numberToAdd) == 1:
-                        for i in range (0+z, 3+z):
-                            for j in range (6, 9):
-                                if sudoku[i][j] == ' ':
-                                    numbersLeft -= 1
-                                    sudoku[i][j] = numberToAdd[0]
-                                    repeat = True
-                                    
-
-
-                for z in range(3):
-                    numberToAdd = list(numbers)
-                    for i in range (0+z, 9, 3):
-                        for j in range (0, 9, 3):
-                            if sudoku[i][j] != ' ':
-                                numberToAdd.remove(sudoku[i][j])
-                    if len(numberToAdd) == 1:
-                        for i in range (0+z, 9, 3):
-                            for j in range (0, 9, 3):
-                                if sudoku[i][j] == ' ':
-                                    numbersLeft -= 1
-                                    sudoku[i][j] = numberToAdd[0]
-                                    repeat = True
-
-
-                    numberToAdd = list(numbers)        
-                    for i in range (0+z, 9, 3):
-                        for j in range (1, 9, 3):
-                            if sudoku[i][j] != ' ':
-                                numberToAdd.remove(sudoku[i][j])
-                    if len(numberToAdd) == 1:
-                        for i in range (0+z, 9, 3):
-                            for j in range (1, 9, 3):
-                                if sudoku[i][j] == ' ':
-                                    numbersLeft -= 1
-                                    sudoku[i][j] = numberToAdd[0]
-                                    repeat = True
-
-
-                    numberToAdd = list(numbers)
-                    for i in range (0+z, 9, 3):
-                        for j in range (2, 9, 3):
-                            if sudoku[i][j] != ' ':
-                                numberToAdd.remove(sudoku[i][j])
-                    if len(numberToAdd) == 1:
-                        for i in range (0+z, 9, 3):
-                            for j in range (2, 9, 3):
-                                if sudoku[i][j] == ' ':
-                                    numbersLeft -= 1
-                                    sudoku[i][j] = numberToAdd[0]
-                                    repeat = True
-
-
-            except:
-                
+            
                 numbersLeft = savedNumbersLeft
                 sudoku = copy.deepcopy(testSudoku)
                 while True:
@@ -158,43 +71,73 @@ def SolveSudoku(sudoku):
                             break
                 sudoku[int(i)][int(j)] = str(number)
                 numbersLeft -= 1
-                repeat = True
+                numberWasAdded = True
                 continue
+        
+            except UnboundLocalError:
                 
-            if repeat == False:
-                for i in range(9):
-                    for j in range(9):
-                         if avaiblePlaces[str(i)+str(j)] == 1:
-                            sudoku[i][j] = avaibleNumbers[str(i)+str(j)]
-                            numbersLeft -= 1
-                            repeat = True
-
-                if numbersLeft == 0:
-                    return sudoku
-
-                if repeat == False:
-                    if pierwszyRaz:
-                        savedNumbersLeft = numbersLeft
-                        pierwszyRaz = False
-                        testSudoku = []
-                        testSudoku = copy.deepcopy(sudoku)
-                    else:
-                        q = 1
-                        if int(q) == 2:
-                            sudoku = []
-                            sudoku = copy.deepcopy(testSudoku)
-                            numbersLeft = savedNumbersLeft
-                    
-                    while True:
+                return 'Cannot solve'
                         
-                        i = random.randint(0, 8)
-                        j = random.randint(0, 8)                   
-                        if sudoku[int(i)][int(j)] == ' ':
-                            number = random.randint(1, 9)
-                            break
-                        
-                    sudoku[int(i)][int(j)] = str(number)
+        for number in numbers:
+            for i in range (9):
+                if sudoku[i].count(number) == 0:
+                    places = 0
+                    placei = 0
+                    placej = 0
+                    for j in range (9):
+                        if sudoku[i][j] == ' ':
+                            works = True
+                            
+                            for i1 in range (groups[i//3][0], groups[i//3][2]+1):
+                                for j1 in range(groups[j//3][0], groups[j//3][2]+1):
+                                    if sudoku[i1][j1] == number:
+                                        works = False
+
+                            for i2 in range (i%3, 9, 3):
+                                for j2 in range(j%3, 9, 3):
+                                    if sudoku[i2][j2] == number:
+                                        works = False
+                            if works:
+                                avaiblePlaces[str(i)+str(j)] += 1
+                                avaibleNumbers[str(i)+str(j)] = number
+                                places += 1
+                                placei = i
+                                placej = j
+
+                    if places == 1:
+                        numberWasAdded = True
+                        sudoku[placei][placej] = number
+                        numbersLeft -= 1
+
+        for i in range(9):
+            for j in range(9):
+                if avaiblePlaces[str(i)+str(j)] == 1 and sudoku[i][j] == ' ':
+                    sudoku[i][j] = avaibleNumbers[str(i)+str(j)]
                     numbersLeft -= 1
-                    repeat = True            
+                    numberWasAdded = True
+         
+        if numberWasAdded == False:
 
-    return sudoku
+            if numbersLeft == 0:
+                return sudoku
+
+            if numberWasAdded == False:
+                if firstTime:
+                    savedNumbersLeft = numbersLeft
+                    firstTime = False
+                    testSudoku = []
+                    testSudoku = copy.deepcopy(sudoku)
+                
+                while True:
+                    
+                    i = random.randint(0, 8)
+                    j = random.randint(0, 8)                   
+                    if sudoku[int(i)][int(j)] == ' ':
+                        number = random.randint(1, 9)
+                        break
+                    
+                sudoku[int(i)][int(j)] = str(number)
+                numbersLeft -= 1
+                numberWasAdded = True
+                
+    return 'Cannot solve'

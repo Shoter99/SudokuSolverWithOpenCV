1b5d338873736945aa9147fb750f9bc41ea9044c
Shoter99 <shoter998@gmail.com>
2022-05-15 10:22:27 +0200

    changes to server, saving pictures

--- Backend/main.py
+++ Backend/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, File, UploadFile
+from random import randint
 from pydantic import BaseModel
 from PythonScripts.Sudoku import SolveSudoku
 import json
@@ -11,13 +12,21 @@ class Board(BaseModel):
 def home(board: Board):
     
     # print(SolveSudoku(res["board"]))
-    return SolveSudoku(board.board)
+    res = SolveSudoku(board.board)
+    if !res:
+        return {'error': 'Something went wrong! Try again'}
+    if res == "Cannot Solve":
+        return {'error': 'Sorry Sudoku is not solvable, try different one'}
+    return res
 @app.get('/api/test')
 def test():
     return 'test'
 
 @app.post('/api/read_numbers')
 def read_numbers(photo: UploadFile):
+    rand = randint(1000)
+    with open(f'image{rand}', 'wb') as img:
+        img.write(photo)
     return "File sent"
 
 # [[" ", "2", " ", "4", " ", "6", " ", " ", " "], [" ", " ", " ", " ", " ", "5", " ", "7", " "], [" ", " ", " ", "1", " ", " ", " ", "3", " "], [" ", "1", " ", "8", " ", "2", " ", "9", " "], [" ", " ", "2", " ", "5", " ", " ", " ", " "], [" ", " ", " ", " ", " ", "7", "8", " ", " "], [" ", " ", " ", "5", " ", "8", "9", " ", " "], ["4", " ", " ", " ", " ", "6", " ", " ", " "], [" ", " ", "1", "4", " ", " ", " ", " ", " "]]
